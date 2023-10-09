0b5db192ab0eba5faa9a493034c328494e57969d
satishnavjivan <satish.dharavia@multicodes.in>
2023-10-09 10:34:42 +0530

    update 91023

--- src/components/categories/category.js
+++ src/components/categories/category.js
@@ -14,7 +14,14 @@ const Category = ( { category } ) => {
 	return (
 		<div className="mt-4 mb-8 px-3 w-full overflow-hidden sm:w-1/2 md:w-1/3 xl:w-1/4">
 			<Link href={ `/categories/${ category?.slug }`} >
-				
+				<a>
+					{/*}<Image
+						sourceUrl={ category.image?.src }
+						altText={ category?.name ?? '' }
+						title={ category?.name ?? '' }
+						width="380"
+						height="380"
+					/>{*/}
 					<Image
 						sourceUrl={ category.category_icon}
 						altText={ category?.name ?? '' }
@@ -23,7 +30,7 @@ const Category = ( { category } ) => {
 						height="100"
 					/>
 					<h6 className="font-bold uppercase my-2 tracking-0.5px">{ category?.name ?? '' }</h6>
-				
+				</a>
 			</Link>
 		</div>
 	)
