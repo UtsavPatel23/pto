7b70cbedf6804bbfb9196f9d8f705f000742d460
UtsavPatel23 <utsav.patel@multicodes.in>
2023-10-30 14:13:42 +0530

    tracking Order

--- pages/my-account/edit-account.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const editaccount = () => {
-  return (
-    <div>edit-account</div>
-  )
-}
-
-export default editaccount
--- pages/my-account/rewards.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const rewards = () => {
-  return (
-    <div>rewards</div>
-  )
-}
