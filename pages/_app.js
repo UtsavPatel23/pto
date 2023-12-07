75f9dd217dd53f4a211c0bde966d8c14802add84
satishnavjivan <satish.dharavia@multicodes.in>
2023-12-07 16:36:56 +0530

    Update 07-12-2023 pagination

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../src/styles/index.scss';
 
 import Router from 'next/router';
 import NProgress from 'nprogress';
-//import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react"
 
 NProgress.configure( { showSpinner: false } );
 Router.events.on( 'routeChangeStart', () => NProgress.start() );
@@ -13,10 +13,9 @@ Router.events.on( 'routeChangeError', () => NProgress.done() );
 export default function MyApp({
   Component, pageProps: { session, ...pageProps }
 }) {
-  /*return (
+  return (
     <SessionProvider session={session}>
       <Component {...pageProps}/>
     </SessionProvider>
-  )*/
-  return <Component {...pageProps} />
+  )
 }
