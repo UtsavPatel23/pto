8df7639c3cf2166abe4dbe21bc0ff46350da3db2
satishnavjivan <satish.dharavia@multicodes.in>
2024-01-10 14:44:27 +0530

    Update 10-1-2024

--- pages/api/order/create-order.js
+++ pages/api/order/create-order.js
@@ -8,30 +8,6 @@ const api = new WooCommerceRestApi( {
 	version: "wc/v3"
 } );
 
-// pages/api/example.js
-import axios from 'axios';
-import Cors from 'cors';
-
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD','POST'],
-});
-
-
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-      return resolve(result);
-    });
-  });
-}
-
 /**
  * Create order endpoint.
  *
@@ -43,8 +19,7 @@ function runMiddleware(req, res, fn) {
  * @return {Promise<{orderId: string, success: boolean, error: string}>}
  */
 export default async function handler( req, res ) {
-	 // Run the middleware
-	 await runMiddleware(req, res, cors);
+	
 	const responseData = {
 		success: false,
 		orderId: '',
